15ee5aedf8743eadd0a1972425c5590d3b6e4d1b
Justin Horein <justinhorein@gmail.com>
2022-02-14 16:11:05 -0800

    add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,38 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 function App() {
+  
+  return (
+  <div className="App">
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Deck</Link>
+          </li>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/" element={<Deck/>}/>
+        <Route path="/search" element={<Search/>}/>
+      </Routes>
+    </Router>
+  </div>
+  );
+}
+
+function Deck() {
   const [cards, setCards] = useState([])
 
   useEffect(() => {
@@ -16,18 +46,23 @@ function App() {
     
     fetchData();
   }, [])
-  
+
   return (
-  <div className="App">
-    <p>yes</p>
+    <div>
+      <div>Deck</div>
 
-    {cards.map((element) => (
-      <img key={element[1].id} src={element[1].img}></img>
-    ))
-    }
-
-  </div>
-  );
+      {cards.map((element) => (
+        <img key={element[1].id} src={element[1].img}></img>
+      ))
+      }
+    </div>
+  )
 }
+
+const Search = () => (
+  <div>
+    <h1>Search</h1>
+  </div>
+)
 
 export default App;
